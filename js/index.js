3bb127bbdb98647d9adfe8276558f0878c335a83
Karina Kolesnik <kerryks@ya.ru>
2019-12-12 21:05:11 +0300

    menu with js, adaptive section slider

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@
 //   this.classList.toggle('hamburger-menu--active')
 // }
 
-const button = document.querySelector ('#hamburger-menu');
-const menu = document.querySelector ('#overlay-menu');
+const button = document.querySelector ('#ham-menu');
+const menu = document.querySelector ('#menu');
 const body = document.querySelector ('body');
 function toggleMenu() {
   button.classList.toggle('hamburger-menu--active');
-  menu.classList.toggle('overlay-menu--active');
+  menu.classList.toggle('overlay-menu__wrap--active');
   body.classList.toggle('menu-open');
 }
 button.addEventListener('click', toggleMenu);
